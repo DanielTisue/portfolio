209a252f22b82fb106ecf0be0ce3b20007a29a4f
DanielTisue <d.tisue@gmail.com>
2019-08-20 10:14:31 -0500

    delete un used routes

--- app.js
+++ app.js
@@ -8,18 +8,10 @@ app.get("/", function (req, res){
     res.render("index");
 })
 
-app.get("/rwamainview", function(req, res){
-    res.render("rwamain");
-})
-
-app.get("/rwachristmasview", function(req, res){
-    res.render("rwachristmas");
-})
-
-app.listen(3000, function(){
-    console.log("server started");
-});
-
-// app.listen(process.env.PORT, process.env.ID, function(){
-//     console.log("Server has Started!");
+// app.listen(3000, function(){
+//     console.log("server started");
 // });
+
+app.listen(process.env.PORT, process.env.ID, function(){
+    console.log("Server has Started!");
+});
