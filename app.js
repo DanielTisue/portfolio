489f85bbb438b5db2ec6b640e5e0c0aa0e8ff11b
DanielTisue <d.tisue@gmail.com>
2019-08-20 10:12:29 -0500

    project update

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.get("/rwachristmasview", function(req, res){
     res.render("rwachristmas");
 })
 
-app.listen(process.env.PORT, process.env.ID, function(){
-    console.log("Server has Started!");
+app.listen(3000, function(){
+    console.log("server started");
 });
+
+// app.listen(process.env.PORT, process.env.ID, function(){
+//     console.log("Server has Started!");
+// });
